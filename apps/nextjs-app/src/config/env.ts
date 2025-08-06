96f519a4f7dcc667deda7c45b1b6c6ae34a2224e
xiu <gaoxiu333@gmail.com>
2025-08-06 10:53:35 +0800

    fix: build

--- apps/nextjs-app/src/config/env.ts
+++ apps/nextjs-app/src/config/env.ts
@@ -3,18 +3,18 @@ import * as z from 'zod';
 
 const createEnv = () => {
   const envSchema = z.object({
-    API_URL: z.string().url().default('/').optional(),
+    API_URL: z.string().default('/').optional(),
     NODE_ENV: z.string().optional(),
-    MOCK_PORT: z
-      .string()
-      .optional()
-      .default('9090')
-      .transform((val) => parseInt(val)),
+    // MOCK_PORT: z
+    //   .string()
+    //   .optional()
+    //   .default('9090')
+    //   .transform((val) => parseInt(val)),
   });
   const envVars = {
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NODE_ENV: process.env.NEXT_PUBLIC_DEPLOY_ENV,
-    MOCK_PORT: process.env.MOCK_PORT,
+    API_URL: '',
+    NODE_ENV: process.env.NODE_ENV,
+    // MOCK_PORT: process.env.MOCK_PORT,
   };
   const parsedEnv = envSchema.safeParse(envVars);
   if (!parsedEnv.success) {
